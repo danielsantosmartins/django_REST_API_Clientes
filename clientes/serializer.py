2f374b394adae05d7631f3119ec0e699149ca0cb
Daniel Martins <danielsantosmartins.dev@gmail.com>
2024-07-07 12:02:41 -0300

    Update: incluida validação no serializer

--- clientes/serializer.py
+++ clientes/serializer.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClientesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
+    def validade(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':"O CPF deve ter 11 dígitos"})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':"Não inclua números nesse campo"})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':"O RG deve ter 9 digitos"})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':"O celular deve ter no minimo 11 digitos"})
+        return data
