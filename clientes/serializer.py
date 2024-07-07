11c50521f05ed0899eab1b701367010c0e4497b0
Daniel Martins <danielsantosmartins.dev@gmail.com>
2024-07-07 12:33:04 -0300

    Update: incluida validação de CPF  e celular

--- clientes/serializer.py
+++ clientes/serializer.py
@@ -8,11 +8,12 @@ class ClientesSerializer(serializers.ModelSerializer):
         fields = '__all__'
     def validade(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf':"O CPF deve ter 11 dígitos"})
+            raise serializers.ValidationError({'cpf':"O número de CPF é inválido"})
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome':"Não inclua números nesse campo"})
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg':"O RG deve ter 9 digitos"})
         if not celular_valido(data['celular']):
-            raise serializers.ValidationError({'celular':"O celular deve ter no minimo 11 digitos"})
+            raise serializers.ValidationError({'celular':"O numero de celular deve seguir este modelo: 11 91234-1234. (respeitando espaços e traços)"})
         return data
+    
